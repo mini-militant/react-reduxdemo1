a93887571460128e84a2a8a9d03f76c9c1fc1346
mini-militant <shailesh.coolcalm007@gmail.com>
2019-05-21 17:35:26 +0530

    2 files modified, 1 added

--- index.js
+++ index.js
@@ -3,7 +3,27 @@ import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
 import ReduxDemo from "./ReduxDemo" 
+import Counter from "./Counter" 
+import {createStore} from 'redux'
 
+//reducer needs to return the initial value when called for the first time
+const initialState={
+  count:0
+};
+
+//it takes the current state and an action and returns NewState
+function reducer(state=initialState,action){
+  console.log('reducer',state,action);
+  return state;
+}
+
+const store=createStore(reducer);
+{
+  type:"ADD_ITEM"
+  item:"APPLE"
+}
+//every call to dispatch results in call to reducer.
+store.dispatch({type:"ADD_ITEM"})
 
 class App extends Component {
   constructor() {
@@ -17,7 +37,8 @@ class App extends Component {
     return (
       <div>
         <Hello name={this.state.name} />
-        <ReduxDemo/>
+        
+        <Counter/>
       </div>
     );
   }
